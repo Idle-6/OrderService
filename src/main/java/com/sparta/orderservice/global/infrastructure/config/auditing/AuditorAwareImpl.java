4b5b07bf403fa484354743bc76c111790ec221f5
jo-hseo <ellejo1110@gmail.com>
2025-10-02 22:50:38 +0900

    Add: Auditing 설정 임시 저장

---
+++ src/main/java/com/sparta/orderservice/global/infrastructure/config/auditing/AuditorAwareImpl.java
@@ -0,0 +1,22 @@
+package com.sparta.orderservice.global.infrastructure.config.auditing;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(authentication.getPrincipal().getId());
+    }
+}
