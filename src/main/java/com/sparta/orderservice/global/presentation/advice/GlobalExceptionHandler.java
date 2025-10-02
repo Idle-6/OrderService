44cb10b1b1b99c90d98a3e18f438e2aac9d5708f
jo-hseo <ellejo1110@gmail.com>
2025-10-02 13:00:04 +0900

    fix Exception 핸들러 추가

--- src/main/java/com/sparta/orderservice/global/presentation/advice/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/orderservice/global/presentation/advice/GlobalExceptionHandler.java
@@ -1,9 +1,19 @@
 package com.sparta.orderservice.global.presentation.advice;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<String> handleException(Exception ex){
+        log.error(ex.getMessage());
+
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 }
