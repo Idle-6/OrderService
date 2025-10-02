a5be5904595173558cfa0e28c61ec923ac635b93
Jieun <dlwldms0895@gmail.com>
2025-10-02 12:36:45 +0900

    add : UserController 및 Dto 생성
    (cherry picked from commit d327cc70ac26a0a92263f940fa8029d12c1871bc)

---
+++ src/main/java/com/sparta/orderservice/user/presentation/dto/ReqSignupDtoV1.java
@@ -0,0 +1,28 @@
+package com.sparta.orderservice.user.presentation.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReqSignupDtoV1 {
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String address;
+
+    @NotBlank
+    private String role;
+
+
+}
