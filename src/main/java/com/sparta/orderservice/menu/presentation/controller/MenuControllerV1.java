8d8c32c17952f25326a03306ac6d5f512bb22023
jo-hseo <ellejo1110@gmail.com>
2025-10-05 11:21:55 +0900

    add : 메뉴 리스트 조회 controller, dto 구현 (with. 더미데이터)

--- src/main/java/com/sparta/orderservice/menu/presentation/controller/MenuControllerV1.java
+++ src/main/java/com/sparta/orderservice/menu/presentation/controller/MenuControllerV1.java
@@ -2,11 +2,15 @@ package com.sparta.orderservice.menu.presentation.controller;
 
 import com.sparta.orderservice.menu.presentation.dto.request.ReqMenuCreateDtoV1;
 import com.sparta.orderservice.menu.presentation.dto.response.ResMenuCreateDtoV1;
+import com.sparta.orderservice.menu.presentation.dto.response.ResMenuGetByStoreIdDtoV1;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -26,17 +30,31 @@ public class MenuControllerV1 {
                 .build();
     }
 
-//    @GetMapping("{storeId}/menu")
-//    public List<ResMenuGetByStoreIdDtoV1> getMenuListByStoreId(
-//            @PathVariable UUID storeId,
-//            @RequestParam("page") int page,
-//            @RequestParam("size") int size,
-//            @RequestParam("sortBy") String sortBy,
-//            @RequestParam("isAsc") boolean isAsc
-//    ) {
-//        return "Menu retrieved";
-//    }
-//
+    @GetMapping("{storeId}/menu")
+    public Page<ResMenuGetByStoreIdDtoV1> getMenuListByStoreId(
+            @PathVariable UUID storeId,
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
+        Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
+        Sort sort = Sort.by(direction, sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+
+        List<ResMenuGetByStoreIdDtoV1> menuList = Arrays.asList(
+                new ResMenuGetByStoreIdDtoV1(UUID.randomUUID(), "자장면", "", 9000, true),
+                new ResMenuGetByStoreIdDtoV1(UUID.randomUUID(), "짬뽕", "신라면 정도 매움", 10000, true),
+                new ResMenuGetByStoreIdDtoV1(UUID.randomUUID(), "탕수육", "쫄깃 바삭한 튀김", 15000, true),
+                new ResMenuGetByStoreIdDtoV1(UUID.randomUUID(), "우육면", "", 9000, false),
+                new ResMenuGetByStoreIdDtoV1(UUID.randomUUID(), "팔보채", "", 15000, true)
+        );
+
+        Page<ResMenuGetByStoreIdDtoV1> productList = new PageImpl<>(menuList, pageable, menuList.size());;
+
+        return productList;
+    }
+
 //    @GetMapping("{menuId}")
 //    public ResMenuGetDtoV1 getMenuById(@PathVariable("menuId") UUID menuId) {
 //        return "Menu retrieved";
