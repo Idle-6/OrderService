27d57c96fa94a7dc23e6fdef83c202ced0563660
jo-hseo <ellejo1110@gmail.com>
2025-10-07 22:27:44 +0900

    fix : getCurrentAuditor 메소드 사용자 정보 가져오는 방법 수정 (Spring Security -> 토큰)

--- src/main/java/com/sparta/orderservice/global/infrastructure/config/auditing/AuditorAwareImpl.java
+++ src/main/java/com/sparta/orderservice/global/infrastructure/config/auditing/AuditorAwareImpl.java
@@ -1,9 +1,15 @@
 package com.sparta.orderservice.global.infrastructure.config.auditing;
 
+import com.sparta.orderservice.auth.infrastructure.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import java.util.Objects;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<Long> {
@@ -11,12 +17,24 @@ public class AuditorAwareImpl implements AuditorAware<Long> {
     @Override
     public Optional<Long> getCurrentAuditor() {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String accessToken = "";
+        JwtUtil jwtUtil = new JwtUtil();
 
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return Optional.empty();
+        HttpServletRequest servletRequest = (
+                (ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())
+        ).getRequest();
+
+        Cookie[] cookies = servletRequest.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(JwtUtil.AUTHORIZATION_HEADER)) {
+                accessToken = cookie.getValue();
+            }
         }
 
-        return Optional.of(authentication.getPrincipal().getId());
+        String at = jwtUtil.substringToken(accessToken);
+        Claims info = jwtUtil.getUserInfoFromToken(at);
+        String username = info.getSubject();
+        //return Optional.of(userRepository.findByUsername(username).userPk);
+        return Optional.of(Long.parseLong(username));
     }
 }
