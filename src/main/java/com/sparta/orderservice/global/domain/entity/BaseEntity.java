54bd66812264f98419df54b44da283645bb636f0
jo-hseo <ellejo1110@gmail.com>
2025-10-12 23:25:13 +0900

    fix : 어노테이션 추가

--- src/main/java/com/sparta/orderservice/global/domain/entity/BaseEntity.java
+++ src/main/java/com/sparta/orderservice/global/domain/entity/BaseEntity.java
@@ -3,7 +3,10 @@ package com.sparta.orderservice.global.domain.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,8 +14,11 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@SuperBuilder
 @EntityListeners(AuditingEntityListener.class)
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity {
 
     @CreatedDate
