50c54660d9e4fff426b1b14324673ee90b6aa62f
jo-hseo <ellejo1110@gmail.com>
2025-10-12 23:13:24 +0900

    fix : GenerationType.IDENTITY -> GenerationType.UUID

--- src/main/java/com/sparta/orderservice/menu/domain/entity/MenuEntity.java
+++ src/main/java/com/sparta/orderservice/menu/domain/entity/MenuEntity.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 public class MenuEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private String name;
