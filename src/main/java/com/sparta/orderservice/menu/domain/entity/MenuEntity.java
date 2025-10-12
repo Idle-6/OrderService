ca9b04ba4207d1a9d4fe32809e5d47dba2d40c78
jo-hseo <ellejo1110@gmail.com>
2025-10-12 23:15:47 +0900

    fix : 어노테이션 추가

--- src/main/java/com/sparta/orderservice/menu/domain/entity/MenuEntity.java
+++ src/main/java/com/sparta/orderservice/menu/domain/entity/MenuEntity.java
@@ -1,11 +1,15 @@
 package com.sparta.orderservice.menu.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "menu")
 @Table(name = "menu")
 public class MenuEntity {
