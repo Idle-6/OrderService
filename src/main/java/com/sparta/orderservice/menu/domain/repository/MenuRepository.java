bb969958051ff40c4b79b68930849a47f118c408
jo-hseo <ellejo1110@gmail.com>
2025-10-14 11:28:07 +0900

    add : MenuRepositoryTest 생성

--- src/main/java/com/sparta/orderservice/menu/domain/repository/MenuRepository.java
+++ src/main/java/com/sparta/orderservice/menu/domain/repository/MenuRepository.java
@@ -3,7 +3,9 @@ package com.sparta.orderservice.menu.domain.repository;
 import com.sparta.orderservice.menu.domain.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface MenuRepository extends JpaRepository<MenuEntity, UUID> {
+    Optional<MenuEntity> findByName(String name);
 }
