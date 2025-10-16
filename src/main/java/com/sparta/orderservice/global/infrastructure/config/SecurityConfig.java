827e8d5263b2c214b0ed4e7b1c19617b8d6d3007
ur <dorosi150@gmail.com>
2025-10-16 14:12:24 +0900

    fix: hasRole -> hasAuthority

--- src/main/java/com/sparta/orderservice/global/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/sparta/orderservice/global/infrastructure/config/SecurityConfig.java
@@ -78,9 +78,9 @@ public class SecurityConfig {
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
                         .requestMatchers("/v1/auth/**").permitAll() // '/v1/auth/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("/v1/users/sign-up").permitAll() // 회원가입 요청 접근 허가
-                        .requestMatchers(HttpMethod.POST, "/v1/categorys/**").hasRole(UserRoleEnum.ADMIN.getAuthority())
-                        .requestMatchers(HttpMethod.PATCH, "/v1/categorys/**").hasRole(UserRoleEnum.ADMIN.getAuthority())
-                        .requestMatchers(HttpMethod.DELETE, "/v1/categorys/**").hasRole(UserRoleEnum.ADMIN.getAuthority())
+                        .requestMatchers(HttpMethod.POST, "/v1/categorys/**").hasAuthority(UserRoleEnum.ADMIN.getAuthority())
+                        .requestMatchers(HttpMethod.PATCH, "/v1/categorys/**").hasAuthority(UserRoleEnum.ADMIN.getAuthority())
+                        .requestMatchers(HttpMethod.DELETE, "/v1/categorys/**").hasAuthority(UserRoleEnum.ADMIN.getAuthority())
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
 
