0b088134f3a20f56a6fa28565548c4343679c756
fleurnari <fleur_nr@naver.com>
2025-10-17 16:43:02 +0900

    fix : 결제 레포지토리 잘못 커밋된 부분 수정

--- src/main/java/com/sparta/orderservice/payment/domain/repository/PaymentRepository.java
+++ src/main/java/com/sparta/orderservice/payment/domain/repository/PaymentRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID>, CustomPaymentRepository {
-    UUID paymentId(UUID paymentId);
 }
